ff04c2fc763240a71a0a8217d8df3ddaa1514036
jordanperrin <93149223+jordanperrin@users.noreply.github.com>
2024-09-10 14:14:04 -0400

    finsihed drawing out court

--- api/index.py
+++ api/index.py
@@ -55,20 +55,46 @@ def draw_court(ax=None, color="blue", lw=1, outer_lines=False):
 
     backboard = Rectangle((-30, -12.5), 60, 0, linewidth=lw, color=color)
 
-    court_elements = [hoop]
+    outer_box = Rectangle((-80, -47.5), 160, 190, linewidth=lw, color = color, fill = False)
+
+    inner_box = Rectangle((-60, -47.5), 120, 190, linewidth=lw, color = color, fill = False)
+
+    top_key = Arc((0, 142.5), 120, 120, theta1=0, theta2=360, linewidth=lw, color = color, fill=False)
+
+    restricted = Arc((0,0), 80,80, theta1=0, theta2=180, linewidth=lw, color=color, fill=False )
+
+    #Three Pointer
+    corner_three_right = Rectangle((-220, -47.5), 0, 140, linewidth=lw, color=color) 
+    corner_three_left = Rectangle((220, -47.5), 0, 140, linewidth=lw, color=color) 
+
+    three_arc = Arc((0,0), 475, 475, theta1=22, theta2=158, linewidth=lw, color=color)
+
+    #HalfCourt
+    center_outer_arc = Arc((0,422.5), 120, 120, theta1=180, theta2=0, linewidth=lw, color=color)
+    inner_outer_arc = Arc((0,422.5), 40, 40, theta1=180, theta2=0, linewidth=lw, color=color)
+
+
+    court_elements = [hoop, backboard, outer_box, inner_box, top_key, restricted, corner_three_right, corner_three_left,three_arc,center_outer_arc, inner_outer_arc]
+
+    outer_lines = True
+
+    if outer_lines:
+        outer_lines = Rectangle((-250, -47.5), 500, 470, linewidth=lw, color=color, fill = False)
+        court_elements.append(outer_lines)
 
     for element in court_elements:
         ax.add_patch(element)
 
 
 if __name__ == "__main__":
-    while(True):
-        xlim= (-250, 250)
-        ylim= (422.5, -47.5)
+   
+    xlim= (-250, 250)
+    ylim= (422.5, -47.5)
 
-        ax = plt.gca()
-        ax.set_xlim(xlim[::-1])
-        ax.set_ylim(ylim[::-1])
+    ax = plt.gca()
+    ax.set_xlim(xlim[::-1])
+    ax.set_ylim(ylim[::-1])
 
-        draw_court(ax)
-        plt.show()
+    draw_court(ax)
+    
+    plt.show()
