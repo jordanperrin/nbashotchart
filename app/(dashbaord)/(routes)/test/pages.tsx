abf8ef83225aa5d9cd9b1fcb9a7ef814ead5eeca
jordanperrin <93149223+jordanperrin@users.noreply.github.com>
2024-10-22 18:13:08 -0400

    wip

---
+++ app/(dashbaord)/(routes)/test/pages.tsx
@@ -0,0 +1,12 @@
+//server component 
+import SearchPlayers from "./SearchPlayers"
+
+const SearchPage = () => {
+  return (
+    <div>
+      <h1>Search Player Here: </h1>
+    </div>
+  )
+}
+
+export default SearchPage
