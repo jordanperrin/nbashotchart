171fa938a4e98d7bf57bccc4611e3153d722746c
Jordan Perrin <perrinjordan@gmail.com>
2024-11-18 22:13:55 -0500

    auto suggest done

--- app/(dashbaord)/(routes)/test/PlayerList.tsx
+++ app/(dashbaord)/(routes)/test/PlayerList.tsx
@@ -1,9 +1,20 @@
+import playersNames from '../../../../public/players_data.json';
+
 
 const PlayerList = async ({query} : {query: string}) => {
+
+  
+
+  const fullNames = playersNames.map((player) => player.full_name);
+
+  const filterPlayers = Array.isArray(fullNames) ? fullNames.filter((player)=>{
+    return player.toLowerCase().includes(query.toLowerCase());
+  }) : [];
+
+  console.log(filterPlayers)
   return (
-    <div>
-      
-    </div>
+    <div></div>
+
   )
 }
 
