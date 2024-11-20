f02e66ca9c570d87f205264702994fec7a5dfd6e
Jordan Perrin <perrinjordan@gmail.com>
2024-11-20 07:40:37 -0500

    added clickable action

--- app/(dashbaord)/(routes)/test/PlayerList.tsx
+++ app/(dashbaord)/(routes)/test/PlayerList.tsx
@@ -1,3 +1,4 @@
+"use client"
 import playersNames from '../../../../public/players_data.json';
 
 
@@ -11,9 +12,34 @@ const PlayerList = async ({query} : {query: string}) => {
     return player.toLowerCase().includes(query.toLowerCase());
   }) : [];
 
-  console.log(filterPlayers)
+  const handleClick = (player : string) =>{
+    console.log(`Clicked on player: ${player}`);
+  };
+
   return (
-    <div></div>
+    <div>
+      {Array.isArray(fullNames) && fullNames.length === 0 && (
+        <p className='mt-4'> No player found</p>
+      )}
+
+      <div className='flex flex-col mt-6'>
+
+        {Array.isArray(fullNames) && 
+          filterPlayers.map((player) =>(
+          <div
+            key ={player}
+            className='flex flex-col cursor-pointer'
+            onClick={() => handleClick(player)}
+            >
+              <div className='flex space-x-6 items-center'>
+                <h2 className=''>{player}</h2>
+              </div>
+
+          </div>
+        ))}
+      </div>
+
+    </div>
 
   )
 }
